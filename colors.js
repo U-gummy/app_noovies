360a747eb0c1069a14eefd250420d364cd7901d3
u-gummy <zoz7353@gmail.com>
2023-04-17 23:28:50 +0900

    :lipstick: Add: 다크모드(useColorScheme) 적용

---
+++ colors.js
@@ -0,0 +1,4 @@
+export const COLORS = {
+  yellow: "#ffa801",
+  black: "#1e272e",
+};
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,16 +1,43 @@
 import React from "react";
+import { Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useColorScheme } from "react-native";
 
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { COLORS } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Movies" component={Movies} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { backgroundColor: isDark ? COLORS.black : "#fff" },
+        tabBarActiveTintColor: isDark ? COLORS.yellow : COLORS.black,
+        tabBarInactiveTintColor: isDark ? "#d2dae2" : "#808e9b",
+        headerStyle: {
+          backgroundColor: isDark ? COLORS.black : "#fff",
+        },
+        headerTitleStyle: {
+          color: isDark ? "#fff" : COLORS.black,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          headerRight: () => (
+            <View>
+              <Text style={{ color: "#fff" }}>삭제</Text>
+            </View>
+          ),
+        }}
+      />
       <Tab.Screen name="Tv" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
