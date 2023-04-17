99d9e01ec34759c1020f050d5244411a0bee048e
u-gummy <zoz7353@gmail.com>
2023-04-17 23:56:52 +0900

    :hammer: Add: 네비게이션 tab bar icon 적용

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useColorScheme } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
@@ -25,6 +26,11 @@ const Tabs = () => {
         headerTitleStyle: {
           color: isDark ? "#fff" : COLORS.black,
         },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+          marginTop: -5,
+        },
       }}
     >
       <Tab.Screen
@@ -36,10 +42,29 @@ const Tabs = () => {
               <Text style={{ color: "#fff" }}>삭제</Text>
             </View>
           ),
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="film-outline" color={color} size={size} />
+          ),
         }}
       />
-      <Tab.Screen name="Tv" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="tv-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
