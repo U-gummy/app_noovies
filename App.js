c8d54641c2432d49585db20067c961726fce789a
u-gummy <zoz7353@gmail.com>
2023-04-25 22:41:57 +0900

    :lipstick: Add: styled components 적용 / theme 추가

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React from "react";
+import { useColorScheme } from "react-native";
+import { ThemeProvider } from "styled-components";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { NavigationContainer } from "@react-navigation/native";
 
 import Root from "./navigation/Root";
+import { darkTheme, lightTheme } from "./styled";
 
 export default function App() {
   const [loaded] = Font.useFonts(Ionicons.font);
+
+  const isDark = useColorScheme() === "dark";
 
   if (!loaded) {
     return <AppLoading />;
   }
 
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.mainBgColor};
+`;
+
+const Title = styled.Text`
+  color: ${({ theme }) => theme.textColor};
+`;
 
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    onPress={() => navigate("Stack", { screen: "Three" })}
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-  >
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })} style={{}}>
+    <Title>Movies</Title>
+  </Btn>
 );
 export default Movies;
