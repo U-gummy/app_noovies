abbc8a4a41ca3763c295367a6d1f2e783d8d0ae0
yougmi <zoz7353@gmail.com>
2023-07-03 09:41:41 +0900

    :sparkles: Add: 검색 페에지 추가, 검색 api 추가

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,10 +1,56 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import styled from "styled-components/native";
+
+import { moviesApi, tvApi } from "../api";
+
+const Container = styled.View``;
+const SearchBar = styled.TextInput`
+  width: 90%;
+  margin: 10px auto;
+  padding: 10px 15px;
+  border-radius: 20px;
+  background-color: #fff;
+`;
 
 const Search = () => {
+  const [query, setQuery] = useState("");
+  const {
+    isLoading: moviesLoading,
+    data: moviesDate,
+    refetch: searchMovies,
+  } = useQuery(["searchMovies", query], moviesApi.search, {
+    enabled: false,
+  });
+  const {
+    isLoading: tvLoading,
+    data: tvDate,
+    refetch: searchTv,
+  } = useQuery(["searchTv", query], tvApi.search, {
+    enabled: false,
+  });
+  const onChangeText = (text: string) => {
+    setQuery(text);
+  };
+  const onSubmit = () => {
+    if (query === "") {
+      return;
+    }
+    searchMovies();
+    searchTv();
+  };
+  console.log("mmmm", moviesLoading, moviesDate);
+  console.log("tttt", tvLoading, tvDate);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Search</Text>
-    </View>
+    <Container>
+      <SearchBar
+        placeholder="Search form Movie or TV Show"
+        placeholderTextColor="gray"
+        returnKeyType="search"
+        onChangeText={onChangeText}
+        onSubmitEditing={onSubmit}
+      />
+    </Container>
   );
 };
 
